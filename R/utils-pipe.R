d4a209c3f54c0493c8b279e8b20ce257abba3f70
Michael-G-Callahan <mgc5166@psu.edu>
2025-11-10 14:15:27 -0500

    fixing collect shards bug (untested -- to be verified on HPC)

---
+++ R/utils-pipe.R
@@ -0,0 +1,7 @@
+#' Internal helpers to bring tidyverse pronouns and pipes into scope.
+#'
+#' @keywords internal
+#' @importFrom magrittr %>%
+#' @importFrom rlang .data
+#' @name internal_tidy_pronouns
+NULL
